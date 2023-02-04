6bf0ce6f357e6be78bc0a5a28936e2343e011780
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2023-02-05 01:40:52 +0300

    Feature: add product gallery

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import WishList from './components/WishList/WishList';
 import Footer from './components/Footer/Footer';
+import ProductGallery from './components/ProductGallery/ProductGallery';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Navbar></Navbar>
     <Home></Home>
     <WishList></WishList>
+    <ProductGallery></ProductGallery>
     <Footer></Footer>
     </>
   );
--- src/components/ProductGallery/ProductGallery.js
+++ src/components/ProductGallery/ProductGallery.js
@@ -1,19 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './ProductGallery.scss';
+import  './ProductGallery.css';
+import tote1 from './Assets/tote1.jpeg';
+import tote2 from './Assets/tote2.jpeg';
+import tote3 from './Assets/tote3.jpeg';
+import tote4 from './Assets/tote4.jpeg';
+import tote7 from './Assets/tote7.jpeg';
+import tote8 from './Assets/tote8.jpeg';
+import tote12 from './Assets/tote12.jpeg';
+import tote13 from './Assets/tote13.jpeg';
+import tote15 from './Assets/tote15.jpeg';
+import tote16 from './Assets/tote16.jpeg';
+import tote17 from './Assets/tote17.jpeg';
+import tote18 from './Assets/tote18.jpeg';
+import tote19 from './Assets/tote19.jpeg';
+import tote20 from './Assets/tote20.jpeg';
+import tote21 from './Assets/tote21.jpeg';
+import tote22 from './Assets/tote22.jpeg';
+import tote23 from './Assets/tote23.jpeg';
+import tote24 from './Assets/tote24.png';
+import tote25 from './Assets/tote29.jpeg';
+import tote26 from './Assets/tote30.jpeg';
 
 const ProductGallery = props => (
-	<div>This is a component called ProductGallery.</div>
+	<div className='overallSection'>
+		<div className='gallery-template'>
+			<img src={tote1} alt="firstStock"/>
+			<img src={tote2} alt="firstStock"/>
+			<img src={tote3} alt="firstStock"/>
+			<img src={tote4} alt="firstStock"/>
+			<img src={tote7} alt="firstStock"/>
+			<img src={tote8} alt="firstStock"/>
+			<img src={tote12} alt="firstStock"/>
+			<img src={tote13} alt="firstStock"/>
+			<img src={tote15} alt="firstStock"/>
+			<img src={tote16} alt="firstStock"/>
+			<img src={tote17} alt="firstStock"/>
+			<img src={tote18} alt="firstStock"/>
+			<img src={tote19} alt="firstStock"/>
+			<img src={tote20} alt="firstStock"/>
+			<img src={tote21} alt="firstStock"/>
+			<img src={tote22} alt="firstStock"/>
+			<img src={tote23} alt="firstStock"/>
+			<img src={tote24} alt="firstStock"/>
+			<img src={tote25} alt="firstStock"/>
+			<img src={tote26} alt="firstStock"/>
+		</div>
+	</div>
 );
 
-// todo: Unless you need to use lifecycle methods or local state,
-// write your component in functional form as above and delete
-// this section. 
-// class ProductGallery extends React.Component {
-//   render() {
-//     return <div>This is a component called ProductGallery.</div>;
-//   }
-// }
 
 const ProductGalleryPropTypes = {
 	// always use prop types!
