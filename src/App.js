34fc9cf38935b4480c54c52056ef91cfe1312be7
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2023-02-04 23:40:52 +0300

    Feature: Add the wishlist page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import WishList from './components/WishList/WishList';
+
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
     <Home></Home>
+    <WishList></WishList>
     </>
   );
 }
--- src/components/WishList/WishList.js
+++ src/components/WishList/WishList.js
@@ -1,24 +1,105 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './WishList.scss';
+// swiper imports 
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const WishList = props => (
-	<div>This is a component called WishList.</div>
-);
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 
-// todo: Unless you need to use lifecycle methods or local state,
-// write your component in functional form as above and delete
-// this section. 
-// class WishList extends React.Component {
-//   render() {
-//     return <div>This is a component called WishList.</div>;
-//   }
-// }
+import { EffectCoverflow, Pagination } from "swiper";
 
-const WishListPropTypes = {
-	// always use prop types!
-};
+// end of swiper 
+import 'swiper/css';
+import Image from './Assets/canvas.jpeg';
+import Image2 from './Assets/canvas1.png';
+import Image3 from './Assets/canvas2.jpeg';
+import Image4 from './Assets/canvas3.jpeg';
+import Image5 from './Assets/canvas4.jpeg'; 
+import './WishList.css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
-WishList.propTypes = WishListPropTypes;
+
+function WishList() {
+
+  return (
+    <>
+		<div className="generalSection">
+			<div className="grid-template" id="firstHalf">
+				<div className="context">
+					<h1>
+						WISHLIST!
+					</h1>
+				</div>
+				<div className="contextContent">
+					<p>
+						These are the products you have in your wishlist.
+					</p>
+				</div>
+				<div className="redirectButton">
+					<button>
+						SEE MORE
+					</button>
+				</div>
+			</div>
+		
+			<div className="grid-template" id="column-temp-2">
+				
+			<Swiper
+				effect={"coverflow"}
+				grabCursor={true}
+				// centeredSlides={true}
+				slidesPerView={"auto"}
+				coverflowEffect={{
+				rotate: 50,
+				stretch: 0,
+				depth: 100,
+				modifier: 1,
+				slideShadows: true,
+				}}
+				pagination={true}
+				modules={[EffectCoverflow, Pagination]}
+				className="mySwiper"
+			>
+				<SwiperSlide>
+				<img src={ Image5} alt="image5"/>
+				</SwiperSlide>
+				<SwiperSlide>
+				<img src={ Image5} alt="image5"/>
+				</SwiperSlide>
+				<SwiperSlide>
+				<img src={ Image4} alt="image4"/>
+				</SwiperSlide>
+				<SwiperSlide>
+				<img src={ Image3} alt="image3"/>
+				</SwiperSlide>
+				<SwiperSlide>
+				<img src={ Image2} alt="image2"/>
+				</SwiperSlide>
+				<SwiperSlide>
+				<img src={Image} alt="image1" />
+				</SwiperSlide>
+			</Swiper>
+			<div className="directLeft">
+				<span>
+				<svg className="checkLeft" xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24"><path fill="currentColor" d="M22 12A10 10 0 0 0 12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10m-2 0a8 8 0 0 1-8 8a8 8 0 0 1-8-8a8 8 0 0 1 8-8a8 8 0 0 1 8 8m-6-5l-5 5l5 5V7Z"/></svg>
+				</span>
+			</div>
+			<div className="directRight">
+				<span>
+				<svg className="checkRight" xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24"><path fill="currentColor" d="M2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2A10 10 0 0 0 2 12m2 0a8 8 0 0 1 8-8a8 8 0 0 1 8 8a8 8 0 0 1-8 8a8 8 0 0 1-8-8m6 5l5-5l-5-5v10Z"/></svg>
+				</span>
+			</div>
+			</div>
+		</div>
+
+
+    </>
+  );
+}
+
 
 export default WishList;
